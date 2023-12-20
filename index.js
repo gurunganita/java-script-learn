08a6dcaab0b514f0052387f8fee9ac1f421b2e3f
gurunganita <gurunganita982@gmail.com>
2023-12-20 11:28:04 +0300

    learned-variables

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello world!");
+// Es5
+var person = "Anita";
+person = "Riya";
+
+//Es6
+let person = "sita";
+person = "shyam";
+
+const person2 = "hari";
+person2 = "anita";
+//ctr + / (comment)
+
+console.log(person2);
